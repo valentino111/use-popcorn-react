61345d9b3bfd9badd275a1961f756ae8c8931294
Valentin Minkovich <valentino111@gmail.com>
2023-08-04 19:48:53 +0300

    115. Building a Reusable Star Rating Component.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+// import "./index.css";
+// import App from "./App";
+import StarRating from "./starRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating maxRaiting={10} />
+    <StarRating maxRaiting={5} />
+    <StarRating />
   </React.StrictMode>
 );
