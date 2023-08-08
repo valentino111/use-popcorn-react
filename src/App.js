f5dca703fb650b3684a9eccf435d0ffc77b4e7b4
Valentin Minkovich <valentino111@gmail.com>
2023-08-08 22:19:07 +0300

    156. Cleaning Up Data Fetching.

--- src/App.js
+++ src/App.js
@@ -42,13 +42,16 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
+
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
 
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           );
 
           if (!res.ok)
@@ -59,9 +62,12 @@ export default function App() {
           if (data.Response === "False") throw new Error("Movie not found.");
 
           setMovies(data.Search);
+          setError("");
         } catch (err) {
           console.error(err.message);
-          setError(err.message);
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -74,6 +80,10 @@ export default function App() {
       }
 
       fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
