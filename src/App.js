408ca00f692178fd8ee688cd7882c74d46e3f360
Valentin Minkovich <valentino111@gmail.com>
2023-08-09 19:43:54 +0300

    167. Refs to Select DOM Elements | fix.

--- src/App.js
+++ src/App.js
@@ -194,7 +194,7 @@ function Search({ query, setQuery }) {
         }
       }
       document.addEventListener("keydown", callback);
-      return () => document.addEventListener("keydown", callback);
+      return () => document.removeEventListener("keydown", callback);
     },
     [setQuery]
   );
